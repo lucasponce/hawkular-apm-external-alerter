07f318c82367533c933d0c471ec46db588cb82ab
Lucas Ponce <ponce.ballesteros@gmail.com>
2016-12-01 10:36:01 +0100

    Add Fraud scenarios tests

--- src/test/java/org/hawkular/apm/alerter/DrlTests.java
+++ src/test/java/org/hawkular/apm/alerter/DrlTests.java
@@ -45,6 +45,45 @@ public class DrlTests {
 
     static String testTenant = "28026b36-8fe4-4332-84c8-524e173a68bf";
 
+    private void runDrlWithEvents(String drl, Collection<Event> events) {
+        final KieBaseConfiguration kieBaseConfiguration = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
+        final KieBase kieBase = new KieHelper().addContent(drl, ResourceType.DRL).build(kieBaseConfiguration);
+        final KieSession kieSession = kieBase.newKieSession();
+        kieSession.addEventListener(new DebugAgendaEventListener());
+        kieSession.addEventListener(new DebugRuleRuntimeEventListener());
+
+        List<String> results = new ArrayList<>();
+        kieSession.setGlobal("results", results);
+
+        final ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(() -> kieSession.fireUntilHalt());
+
+        events.stream().forEach(e -> kieSession.insert(e));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        kieSession.halt();
+        kieSession.dispose();
+
+        Assert.assertEquals(1, results.size());
+        Assert.assertTrue(results.contains("user1"));
+
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(10, TimeUnit.MILLISECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+
     String uuid() {
         return UUID.randomUUID().toString();
     }
@@ -114,43 +153,109 @@ public class DrlTests {
                 "   results.add( $accountId ); \n " +
                 " end \n " +
                 " \n ";
-        final KieBaseConfiguration kieBaseConfiguration = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
-        final KieBase kieBase = new KieHelper().addContent(drl, ResourceType.DRL).build(kieBaseConfiguration);
-        final KieSession kieSession = kieBase.newKieSession();
-        kieSession.addEventListener(new DebugAgendaEventListener());
-        kieSession.addEventListener(new DebugRuleRuntimeEventListener());
 
-        List<String> results = new ArrayList<>();
-        kieSession.setGlobal("results", results);
-
-        final ExecutorService executor = Executors.newFixedThreadPool(2);
-        executor.submit(() -> kieSession.fireUntilHalt());
-
-        marketingScenario().stream().forEach(e -> kieSession.insert(e));
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        kieSession.halt();
-        kieSession.dispose();
-
-        Assert.assertEquals(1, results.size());
-        Assert.assertTrue(results.contains("user1"));
-
-        executor.shutdown();
-        try {
-            if (!executor.awaitTermination(10, TimeUnit.MILLISECONDS)) {
-                executor.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Assert.fail(e.getMessage());
-        }
+        runDrlWithEvents(drl, marketingScenario());
     }
 
+    Collection<Event> fraudScenario() {
+        // User1 buys 5 times in < 10 seconds
+        long now = System.currentTimeMillis();
+        Event e1 = new Event(testTenant, uuid(), now, "apm-data-id", "TraceCompletion", "Buy Book");
+        e1.addTag("duration", "1000");
+        e1.addTag("accountId", "user1");
+        e1.addTag("location", "ip1");
+        Event e2 = new Event(testTenant, uuid(), now + 1000, "apm-data-id", "TraceCompletion", "Buy Music");
+        e2.addTag("duration", "2000");
+        e2.addTag("accountId", "user1");
+        e2.addTag("location", "ip1");
+        Event e3 = new Event(testTenant, uuid(), now + 2000, "apm-data-id", "TraceCompletion", "Buy Groceries");
+        e3.addTag("duration", "1500");
+        e3.addTag("accountId", "user1");
+        e3.addTag("location", "ip1");
+        Event e4 = new Event(testTenant, uuid(), now + 3000, "apm-data-id", "TraceCompletion", "Buy VideoGames");
+        e4.addTag("duration", "3000");
+        e4.addTag("accountId", "user1");
+        e4.addTag("location", "ip2");
+        Event e5 = new Event(testTenant, uuid(), now + 4000, "apm-data-id", "TraceCompletion", "Buy VideoGames");
+        e5.addTag("duration", "3000");
+        e5.addTag("accountId", "user1");
+        e5.addTag("location", "ip1");
 
+        // User2 buys 3 times > 10 seconds
+        Event e6 = new Event(testTenant, uuid(), now, "apm-data-id", "TraceCompletion", "Buy Book");
+        e6.addTag("duration", "1000");
+        e6.addTag("accountId", "user2");
+        e6.addTag("location", "ip3");
+        Event e7 = new Event(testTenant, uuid(), now + 15000, "apm-data-id", "TraceCompletion", "Buy Music");
+        e7.addTag("duration", "2000");
+        e7.addTag("accountId", "user2");
+        e7.addTag("location", "ip4");
+        Event e8 = new Event(testTenant, uuid(), now + 20000, "apm-data-id", "TraceCompletion", "Buy Groceries");
+        e8.addTag("duration", "1500");
+        e8.addTag("accountId", "user2");
+        e8.addTag("location", "ip5");
+
+        Event e11 = new Event(testTenant, uuid(), now, "apm-data-id", "TraceCompletion", "Buy Book");
+        e11.addTag("duration", "1000");
+        e11.addTag("accountId", "user3");
+        e11.addTag("location", "ip10");
+        Event e12 = new Event(testTenant, uuid(), now + 1000, "apm-data-id", "TraceCompletion", "Buy Music");
+        e12.addTag("duration", "2000");
+        e12.addTag("accountId", "user3");
+        e12.addTag("location", "ip10");
+        Event e13 = new Event(testTenant, uuid(), now + 2000, "apm-data-id", "TraceCompletion", "Buy Groceries");
+        e13.addTag("duration", "1500");
+        e13.addTag("accountId", "user3");
+        e13.addTag("location", "ip10");
+        Event e14 = new Event(testTenant, uuid(), now + 3000, "apm-data-id", "TraceCompletion", "Buy VideoGames");
+        e14.addTag("duration", "3000");
+        e14.addTag("accountId", "user3");
+        e14.addTag("location", "ip10");
+        Event e15 = new Event(testTenant, uuid(), now + 4000, "apm-data-id", "TraceCompletion", "Buy VideoGames");
+        e15.addTag("duration", "3000");
+        e15.addTag("accountId", "user3");
+        e15.addTag("location", "ip10");
+
+        return Arrays.asList(e1, e2, e3, e4, e5, e6, e7, e8, e11, e12, e13, e14, e15);
+    }
+
+    @Test
+    public void fraudScenarioTest() {
+        String drl = " import org.hawkular.alerts.api.model.event.Event; \n " +
+                " import java.util.List; \n " +
+                " declare AccountId accountId : String end \n " +
+                " global java.util.List results; \n" +
+                " rule \"Extract accountId\" \n " +
+                " when \n " +
+                "   Event( $accountId : tags[\"accountId\"] != null ) \n " +
+                "   not AccountId ( accountId == $accountId  ) \n " +
+                " then \n" +
+                "   insert( new AccountId ( $accountId ) ); \n " +
+                " end \n " +
+                " \n " +
+                " rule \"Fraud Scenario\" \n " +
+                " when" +
+                "   AccountId ( $accountId : accountId ) \n " +
+                "   accumulate( $event : Event( tags[\"accountId\"] == $accountId, $ctime : ctime, $location : tags[\"location\"] ); \n " +
+                "               $sizeEvents : count( $event ), \n" +
+                "               $firstEvent : min( $ctime ), \n" +
+                "               $lastEvent  : max( $ctime )," +
+                "               $events : collectList( $event )," +
+                "               $locations : collectSet( $location ); \n" +
+                "               $sizeEvents > 1, \n " +
+                "               $lastEvent - $firstEvent < 10000," +
+                "               $locations.size > 1 ) \n " +
+                " then \n " +
+                "   System.out.println(\"Account \" + $accountId + \" has \" + $sizeEvents + " +
+                "                      \" events in \" + ($lastEvent.longValue() - $firstEvent.longValue()) " +
+                "                      + \" ms from these locations: \" + $locations); \n " +
+                "   System.out.println(\"Events: \" + $events); \n " +
+                "   $events.stream().forEach(e -> retract( e )); \n " +
+                "   results.add( $accountId ); \n " +
+                " end \n " +
+                " \n ";
+
+        runDrlWithEvents(drl, fraudScenario());
+    }
 
 }
